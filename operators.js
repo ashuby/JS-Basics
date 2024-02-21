d77342b49dfd460adcdf70eb165e82ff201dd61f
Ashutosh Singh (CW) <Ashutosh.Singh@jda.com>
2024-02-21 12:14:11 +0530

    operators

---
+++ operators.js
@@ -0,0 +1,18 @@
+//Operators
+//Arithmetic operators(+,-,*,/,%,++,--)
+//Logical operators(&&,||,!)
+//Bitwise Operators(&,|,^,<<,>>,>>>)
+var a = 10;
+var b = 20;
+var c = -2;
+console.log(a<<2);
+console.log(b>>2);
+console.log(c);
+console.log(c>>>2);
+//Comparison operator(==,===,<=,>=,!=,>,<)
+//Assignment operator(=,+=,-=,*=,/=,%=,&=,|=,^=)
+console.log(~b);
+var d = -9;
+console.log(~d);
+//Ternary Operator(?)
+//Special operators(rest operator,spread operator)...
